bd86341b9662315fe7b733b05f7c589c638d0ae9
Jose Meza <jcdlega@gmail.com>
2025-03-10 12:17:57 -0600

    Add: Agrega inserts de registros de 7 usuarios, Instrucciones del orden de inserciones de tabla y la imagen del diagrama Entidad-Relacion-Petcalli

--- BD/Script-inserts.sql
+++ BD/Script-inserts.sql
@@ -1,1 +1,13 @@
 -- Aqui insertamos los datos 5 por cada tabla
+
+--Esta es la seccion de insercion  usuarios deacuerdo a el orden de inserción de README.md
+
+INSERT INTO users (name, lastName, email, telephone, password) 
+    VALUES 
+        ('Cesar', 'Millan', 'cesar.millan@pets.com', '123-456-7890', 'DogW!sp1'), 
+        ('Jackson', 'Galaxy', 'jackson.galaxy@pets.com', '987-654-3210', 'CatD@ddy2'), 
+        ('Temple', 'Grandin', 'temple.grandin@pets.com', '555-678-1234', 'An!m4lB3'), 
+        ('Juan', 'Villalobos', 'juan.villalobos@pets.com', '444-222-5678', 'Perr!t05'), 
+        ('Jane', 'Goodall', 'jane.goodall@pets.com', '666-999-0000', 'ChimpR$8'), 
+        ('José', 'Ernesto', 'jose.ernesto@pets.com', '111-333-2222', 'Ec0l0g!a'), 
+        ('Carlos', 'Rodríguez', 'carlos.rodriguez@pets.com', '777-888-9999', 'VetL0v#9');
