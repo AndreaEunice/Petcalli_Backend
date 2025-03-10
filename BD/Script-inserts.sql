413111e99e4ec08e5c2a56755b8f0f2aad9a635b
AnieCG <qfb.anacg@gmail.com>
2025-03-10 15:31:11 -0600

    feat:se crean 5 categorias

--- BD/Script-inserts.sql
+++ BD/Script-inserts.sql
@@ -1,1 +1,8 @@
 -- Aqui insertamos los datos 5 por cada tabla
+INSERT INTO productscategory(id,name) 
+VALUES
+(1, 'Accesorios'),
+(2, 'Ropa'),
+(3, 'Alimento'),
+(4, 'Juguetes'),
+(5, 'Medicamento');
