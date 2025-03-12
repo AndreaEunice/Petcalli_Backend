88fc3335140e32610f13f7111d3aca5d36bbdbf9
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-03-11 18:36:27 -0600

    modificando la estructura de la bd

--- BD/Petcalli_v1.sql
+++ BD/Petcalli_v1.sql
@@ -19,6 +19,36 @@
 -- Table structure for table `address`
 --
 
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `userscol_UNIQUE` (`telephone`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Cesar','Millan','cesar.millan@pets.com','123-456-7890','DogW!sp1'),(2,'Jackson','Galaxy','jackson.galaxy@pets.com','987-654-3210','CatD@ddy2'),(3,'Temple','Grandin','temple.grandin@pets.com','555-678-1234','An!m4lB3'),(4,'Juan','Villalobos','juan.villalobos@pets.com','444-222-5678','Perr!t05'),(5,'Jane','Goodall','jane.goodall@pets.com','666-999-0000','ChimpR$8'),(6,'José','Ernesto','jose.ernesto@pets.com','111-333-2222','Ec0l0g!a'),(7,'Carlos','Rodríguez','carlos.rodriguez@pets.com','777-888-9999','VetL0v#9');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -30,11 +60,12 @@ CREATE TABLE `address` (
   `state` varchar(100) NOT NULL,
   `zip` int NOT NULL,
   `country` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`,`fk_id_user`),
+  PRIMARY KEY (`id`),
   KEY `fk_id_users_idx` (`fk_id_user`),
   CONSTRAINT `fk_id_users` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `address`
@@ -46,6 +77,71 @@ INSERT INTO `address` VALUES (1,1,'Av. Revolucion','Monterrey','Nuevo León',640
 /*!40000 ALTER TABLE `address` ENABLE KEYS */;
 UNLOCK TABLES;
 
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `brand` varchar(45) NOT NULL,
+  `fk_id_petType` int NOT NULL,
+  `image` varchar(300) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `fk_id_category` int NOT NULL,
+  `color` varchar(45) DEFAULT NULL,
+  `size` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_category_idx` (`fk_id_category`),
+  KEY `fk_id_petType_idx` (`fk_id_petType`),
+  CONSTRAINT `fk_id_category` FOREIGN KEY (`fk_id_category`) REFERENCES `productscategory` (`id`),
+  CONSTRAINT `fk_id_petType` FOREIGN KEY (`fk_id_petType`) REFERENCES `pettype` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Mother & Babycat Comida Seca','Royal Canin',2,'https://cdn.royalcanin-weshare-online.io/rz_NTIcBRYZmsWpcJPUx/v56/00030111450036-cf-gs1?w=640&fm=jpg&auto=format,compress',610.00,'Nutricion completa y balanceada para gatos en gestion y lactancia, así como para sus cachorros (de 1 a 4 meses de edad) durante el destete.',3,'NA','1.3 KG'),(2,'Mother & Babycat Comida Seca','Royal Canin',2,'https://cdn.royalcanin-weshare-online.io/rz_NTIcBRYZmsWpcJPUx/v56/00030111450036-cf-gs1?w=640&fm=jpg&auto=format,compress',855.00,'Nutricion completa y balanceada para gatos en gestion y lactancia, así como para sus cachorros (de 1 a 4 meses de edad) durante el destete.',3,'NA','2.6 KG'),(11,'Small Starter Mother & Babydog','Royal Canin',1,'https://cdn.royalcanin-weshare-online.io/f-fQTYcBaPOZra8qVPyg/v71/00030111445148-cf-gs1?w=640&fm=jpg&auto=format,compress',470.00,'Nutrición completa y equilibrada para perros: para la madre pequeña (hasta 10 kg) y sus cachorros. Madre al final de la gestación y durante la lactancia. Cachorros en el destete hasta los 2 meses de edad.',3,'NA','1.1 KG'),(12,'Small Starter Mother & Babydog','Royal Canin',1,'https://cdn.royalcanin-weshare-online.io/f-fQTYcBaPOZra8qVPyg/v71/00030111445148-cf-gs1?w=640&fm=jpg&auto=format,compress',1630.00,'Nutrición completa y equilibrada para perros: para la madre pequeña (hasta 10 kg) y sus cachorros. Madre al final de la gestación y durante la lactancia. Cachorros en el destete hasta los 2 meses de edad.',3,'NA','6.3 KG'),(45,'Bolas Rodadoras para Pequeñas Mascotas','Oxbow',3,'https://www.petco.com.mx/medias/133191.jpg-300ftw?context=bWFzdGVyfHJvb3R8NjI5MTV8aW1hZ2UvanBlZ3xoYjcvaDVmLzk1NzU1MzcwNDk2MzAuanBnfDcxYTU3NmQ0Y2U4MzA2NjFiZGJmMGFlYTg5ODY0OTliZDJlNzFjOGU5YzJmMzRkYTVjZThlNTkxODYwZTMyNTA',161.50,'Oxbow Enriched Life Bolas Rodadoras para Pequeñas Mascotas.',4,'NA','NA');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fk_id_user` int NOT NULL,
+  `amount_products` int NOT NULL,
+  `date_time_order` datetime NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `payment_method` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_user_idx` (`fk_id_user`),
+  CONSTRAINT `fk_id_user` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,2,5,'2025-02-23 05:10:43',802.75,'tarjeta_de_credito'),(2,1,1,'2025-02-18 05:37:11',700.50,'tarjeta_de_debito'),(3,1,1,'2025-02-26 10:23:15',451.25,'tarjeta_de_debito'),(4,1,3,'2025-02-15 08:11:32',484.50,'tarjeta_de_credito'),(5,2,2,'2025-02-22 11:05:28',579.50,'tarjeta_de_debito');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 --
 -- Table structure for table `coupons`
 --
@@ -103,36 +199,6 @@ LOCK TABLES `coupons_has_orders` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `fk_id_user` int NOT NULL,
-  `amount_products` int NOT NULL,
-  `date_time_order` datetime NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
-  `payment_method` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`,`fk_id_user`),
-  KEY `fk_id_user_idx` (`fk_id_user`),
-  CONSTRAINT `fk_id_user` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,2,5,'2025-02-23 05:10:43',802.75,'tarjeta_de_credito'),(2,1,1,'2025-02-18 05:37:11',700.50,'tarjeta_de_debito'),(3,1,1,'2025-02-26 10:23:15',451.25,'tarjeta_de_debito'),(4,1,3,'2025-02-15 08:11:32',484.50,'tarjeta_de_credito'),(5,2,2,'2025-02-22 11:05:28',579.50,'tarjeta_de_debito');
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `pettype`
 --
 
@@ -155,42 +221,6 @@ LOCK TABLES `pettype` WRITE;
 /*!40000 ALTER TABLE `pettype` DISABLE KEYS */;
 INSERT INTO `pettype` VALUES (1,'Perros','Descubre todo sobre los perros, nuestros fieles compañeros. Desde razas, cuidados, alimentación y entrenamiento hasta consejos para su bienestar y salud. Encuentra información útil para brindarles una vida feliz y saludable.'),(2,'Gatos','Explora el fascinante mundo de los gatos. Encuentra información sobre razas, cuidados, alimentación, comportamiento y salud felina. Descubre consejos para mejorar su bienestar y fortalecer el vínculo con tu minino.'),(3,'Conejos','Aprende todo sobre los conejos, adorables y curiosas mascotas. Descubre sus cuidados, alimentación, hábitat, comportamiento y consejos para garantizar su bienestar. Encuentra información útil para brindarles una vida sana y feliz.'),(4,'Aves','Descubre el maravilloso mundo de las aves. Encuentra información sobre especies, cuidados, alimentación, comportamiento y consejos para su bienestar. Aprende cómo proporcionarles un entorno adecuado y disfrutar de su compañía.'),(5,'Otros','Explora información sobre una variedad de mascotas y animales exóticos. Desde roedores y reptiles hasta peces e invertebrados, descubre sus cuidados, alimentación y consejos para brindarles un hogar adecuado y saludable.');
 /*!40000 ALTER TABLE `pettype` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `brand` varchar(45) NOT NULL,
-  `fk_id_petType` int NOT NULL,
-  `image` varchar(300) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `fk_id_category` int NOT NULL,
-  `color` varchar(45) DEFAULT NULL,
-  `size` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`,`fk_id_category`,`fk_id_petType`),
-  KEY `fk_id_category_idx` (`fk_id_category`),
-  KEY `fk_id_petType_idx` (`fk_id_petType`),
-  CONSTRAINT `fk_id_category` FOREIGN KEY (`fk_id_category`) REFERENCES `productscategory` (`id`),
-  CONSTRAINT `fk_id_petType` FOREIGN KEY (`fk_id_petType`) REFERENCES `pettype` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Mother & Babycat Comida Seca','Royal Canin',2,'https://cdn.royalcanin-weshare-online.io/rz_NTIcBRYZmsWpcJPUx/v56/00030111450036-cf-gs1?w=640&fm=jpg&auto=format,compress',610.00,'Nutricion completa y balanceada para gatos en gestion y lactancia, así como para sus cachorros (de 1 a 4 meses de edad) durante el destete.',3,'NA','1.3 KG'),(2,'Mother & Babycat Comida Seca','Royal Canin',2,'https://cdn.royalcanin-weshare-online.io/rz_NTIcBRYZmsWpcJPUx/v56/00030111450036-cf-gs1?w=640&fm=jpg&auto=format,compress',855.00,'Nutricion completa y balanceada para gatos en gestion y lactancia, así como para sus cachorros (de 1 a 4 meses de edad) durante el destete.',3,'NA','2.6 KG'),(11,'Small Starter Mother & Babydog','Royal Canin',1,'https://cdn.royalcanin-weshare-online.io/f-fQTYcBaPOZra8qVPyg/v71/00030111445148-cf-gs1?w=640&fm=jpg&auto=format,compress',470.00,'Nutrición completa y equilibrada para perros: para la madre pequeña (hasta 10 kg) y sus cachorros. Madre al final de la gestación y durante la lactancia. Cachorros en el destete hasta los 2 meses de edad.',3,'NA','1.1 KG'),(12,'Small Starter Mother & Babydog','Royal Canin',1,'https://cdn.royalcanin-weshare-online.io/f-fQTYcBaPOZra8qVPyg/v71/00030111445148-cf-gs1?w=640&fm=jpg&auto=format,compress',1630.00,'Nutrición completa y equilibrada para perros: para la madre pequeña (hasta 10 kg) y sus cachorros. Madre al final de la gestación y durante la lactancia. Cachorros en el destete hasta los 2 meses de edad.',3,'NA','6.3 KG'),(45,'Bolas Rodadoras para Pequeñas Mascotas','Oxbow',3,'https://www.petco.com.mx/medias/133191.jpg-300ftw?context=bWFzdGVyfHJvb3R8NjI5MTV8aW1hZ2UvanBlZ3xoYjcvaDVmLzk1NzU1MzcwNDk2MzAuanBnfDcxYTU3NmQ0Y2U4MzA2NjFiZGJmMGFlYTg5ODY0OTliZDJlNzFjOGU5YzJmMzRkYTVjZThlNTkxODYwZTMyNTA',161.50,'Oxbow Enriched Life Bolas Rodadoras para Pequeñas Mascotas.',4,'NA','NA');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -298,36 +328,6 @@ CREATE TABLE `user rol` (
 LOCK TABLES `user rol` WRITE;
 /*!40000 ALTER TABLE `user rol` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user rol` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `lastName` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telephone` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `userscol_UNIQUE` (`telephone`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Cesar','Millan','cesar.millan@pets.com','123-456-7890','DogW!sp1'),(2,'Jackson','Galaxy','jackson.galaxy@pets.com','987-654-3210','CatD@ddy2'),(3,'Temple','Grandin','temple.grandin@pets.com','555-678-1234','An!m4lB3'),(4,'Juan','Villalobos','juan.villalobos@pets.com','444-222-5678','Perr!t05'),(5,'Jane','Goodall','jane.goodall@pets.com','666-999-0000','ChimpR$8'),(6,'José','Ernesto','jose.ernesto@pets.com','111-333-2222','Ec0l0g!a'),(7,'Carlos','Rodríguez','carlos.rodriguez@pets.com','777-888-9999','VetL0v#9');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
