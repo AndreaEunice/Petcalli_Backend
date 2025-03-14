aa4c2a09d750fa2279b581d0504f574167b3f1dd
Aurelia <aurelia.roblesperez@gmail.com>
2025-03-14 11:12:53 -0600

    se realiza el archivo de UserUtil

---
+++ petcalli_backend 2/src/main/java/mx/petcalli/app/util/UserDataLoader.java
@@ -0,0 +1,23 @@
+package mx.petcalli.app.util;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import mx.petcalli.app.model.User;
+import mx.petcalli.app.repository.UserRepository;
+
+public class UserDataLoader implements CommandLineRunner{
+        @Autowired
+      UserRepository userRepository;
+        @Override
+        public void run(String... args) throws Exception {
+      userRepository.save(new User("Cesar", "Millan", "cesar.millan@pets.com", "123-456-7890", "DogW!sp1"));
+      userRepository.save(new User("Jackson", "Galaxy", "jackson.galaxy@pets.com", "987-654-3210", "CatD@ddy2"));
+      userRepository.save(new User("Temple", "Grandin", "temple.grandin@pets.com", "555-678-1234", "An!m4lB3"));
+      userRepository.save(new User("Juan", "Villalobos", "juan.villalobos", "444-222-5678", "Perr!t05"));
+      userRepository.save(new User("Jane", "Goodall", "jane.goodall", "666-999-0000", "ChimpR$8"));
+      userRepository.save(new User("Jose", "Ernesto", "jose.ernesto@pets.com", "111-333-2222", "Ec0l0g!a"));
+      userRepository.save(new User("Carlos", "Rodriguez", "carlos.rodriguez@pets.com", "777-888-9999", "VetL0v#9"));
+        }
+}
