b111f871de07cefa34a76c4ce0e51f8bb43684b4
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-03-15 01:30:07 -0600

    fix:Modifivando controller y service

--- petcalli_backend 2/src/main/java/mx/petcalli/app/service/impl/PetTypeServiceImpl.java
+++ petcalli_backend 2/src/main/java/mx/petcalli/app/service/impl/PetTypeServiceImpl.java
@@ -1,7 +1,20 @@
 package mx.petcalli.app.service.impl;
 
+import mx.petcalli.app.model.PetType;
 import mx.petcalli.app.service.PetTypeService;
 
 public class PetTypeServiceImpl implements PetTypeService {
+
+	@Override
+	public PetType createPetType(PetType petType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PetType getPetTypeById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }	
