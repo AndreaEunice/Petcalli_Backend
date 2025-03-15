f3e61f51feb0bf4bcdfaa38b77cc508c4d4e9b07
Erika Gonzalez <erikagov0926@gmail.com>
2025-03-14 18:55:15 -0600

    RoleService y RoleServiceImpl creados

--- petcalli_backend 2/src/main/java/mx/petcalli/app/service/RoleService.java
+++ petcalli_backend 2/src/main/java/mx/petcalli/app/service/RoleService.java
@@ -1,12 +1,14 @@
 package mx.petcalli.app.service;
 
+import java.util.Set;
+
 import mx.petcalli.app.model.Role;
 
 public interface RoleService {
 	Role createRole(Role role);
 	Role getRoleById(Integer id);
-	//Role getCustomerByEmail(String email);
-	//Page<Customer> getAllUsers(boolean isActive, int pageNumber, int pageSize);
+
 	Role updateRole(Role role, Integer id);
 	void deleteRole(Integer id);
+
 }
