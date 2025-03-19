c1a80843f2584f42d342b32f228ee828d3d7cbe5
Jose Meza <jcdlega@gmail.com>
2025-03-19 17:44:31 -0600

    Feat:Se agregan las CRUD de productCategory

--- petcalli_backend 2/src/main/java/mx/petcalli/app/controller/impl/ProductCategoryControllerImpl.java
+++ petcalli_backend 2/src/main/java/mx/petcalli/app/controller/impl/ProductCategoryControllerImpl.java
@@ -2,6 +2,12 @@ package mx.petcalli.app.controller.impl;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,48 +17,46 @@ import mx.petcalli.app.service.ProductCategoryService;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v1/productCategory") // localhost:8080/api/v1 
+@RequestMapping("/api/v1/productCategory") // localhost:8080/api/v1/productCateogry
 public class ProductCategoryControllerImpl implements ProductCategoryController {
 
 	private final ProductCategoryService productCategoryService;
 	
-	public ProductCategoryControllerImpl(ProductCategoryService productCategoryService){
-		this.productCategoryService =productCategoryService;
-	}
-	
-	@Override
-	public ProductCategory getProductCategoryById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public ProductCategoryControllerImpl(ProductCategoryService productCategoryService) {
+		this.productCategoryService = productCategoryService;
 	}
 
 	@Override
-	public ResponseEntity<ProductCategory> getProductCategoryByIdUsingResponseEntity(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping("{id}")
+	public ResponseEntity<ProductCategory> getProductCategoryByIdUsingResponseEntity(@PathVariable("id") Integer id) {
+		return ResponseEntity.ok(productCategoryService.getProductCategoryById(id));
 	}
 
 	@Override
-	public ResponseEntity<ProductCategory> createProductCategory(ProductCategory newproductCategory) {
-		// TODO Auto-generated method stub
-		return null;
+	@PostMapping
+	public ResponseEntity<ProductCategory> createProductCategory(@RequestBody ProductCategory newproductCategory) {
+		return ResponseEntity.status(201).body(productCategoryService.createProductCategory(newproductCategory));
 	}
 
 	@Override
+	@GetMapping
 	public ResponseEntity<Iterable<ProductCategory>> getAllProductCategories() {
 		Iterable<ProductCategory> productCategories = productCategoryService.getAllProductCategories();
 		return ResponseEntity.ok( productCategories  );
 	}
 
 	@Override
-	public ResponseEntity<ProductCategory> updateProductCategory(ProductCategory productCategory, Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	@PutMapping("{id}")
+	public ResponseEntity<ProductCategory> updateProductCategory(@RequestBody ProductCategory productCategory,@PathVariable("id") Integer id) {
+		return ResponseEntity.ok(productCategoryService.updateProductCategory(productCategory, id));
 	}
 
 	@Override
-	public ResponseEntity<Void> deleteProductCategory(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> deleteProductCategory(@PathVariable("id") Integer id) {
+		productCategoryService.deleteProductCategory(id);
+		return ResponseEntity.noContent().build();
 	}
+
+
 }
