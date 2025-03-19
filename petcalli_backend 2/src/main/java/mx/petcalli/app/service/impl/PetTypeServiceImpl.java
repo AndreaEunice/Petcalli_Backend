9d69463e6fe867599ab6dab2ffc6f0cf203ed422
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-03-19 17:44:01 -0600

    Ruta obtener todos los petType funcionando

--- petcalli_backend 2/src/main/java/mx/petcalli/app/service/impl/PetTypeServiceImpl.java
+++ petcalli_backend 2/src/main/java/mx/petcalli/app/service/impl/PetTypeServiceImpl.java
@@ -1,13 +1,23 @@
 package mx.petcalli.app.service.impl;
 
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
 import mx.petcalli.app.model.PetType;
+import mx.petcalli.app.repository.PetTypeRepository;
 import mx.petcalli.app.service.PetTypeService;
 
 @Service
 public class PetTypeServiceImpl implements PetTypeService {
 
+	private final PetTypeRepository petTypeRepository;
+	
+	PetTypeServiceImpl(PetTypeRepository petTypeRepository){
+		this.petTypeRepository = petTypeRepository;
+	}
 	@Override
 	public PetType createPetType(PetType petType) {
 		// TODO Auto-generated method stub
@@ -18,6 +28,25 @@ public class PetTypeServiceImpl implements PetTypeService {
 	public PetType getPetTypeById(int id) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public PetType updatePetType(PetType petType, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<PetType> getAllPetTypes() {
+		// TODO Auto-generated method stub
+		Iterable<PetType> petTypesIterable = petTypeRepository.findAll();
+		return new LinkedHashSet<>((Collection<PetType>)petTypesIterable);
+	}
+
+	@Override
+	public void deletePetType(int id) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }	
